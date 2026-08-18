2d8990320f6d3fc6d654bd4b91d8c97b9be13bff
Fenil Patel <fenil.patel0706@gmail.com>
2026-08-18 19:23:09 +0530

    Day1-basic

---
+++ Day1.py
@@ -0,0 +1,8 @@
+# Q1 A store gives free shipping on orders of $50 or more. Write an if/else statement for a variable
+
+order_amount = 50;
+
+if order_amount >= 10: 
+    print("free shipping");
+else:
+    print("Shipping Cost: $5");
