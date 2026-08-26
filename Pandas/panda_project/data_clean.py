8a9a966750e12c30c0dd0893943ceb2468e9c650
Fenil Patel <fenil.patel0706@gmail.com>
2026-08-26 19:51:24 +0530

    dat acleing

--- Pandas/panda_project/data_clean.py
+++ Pandas/panda_project/data_clean.py
@@ -5,11 +5,21 @@ import pandas as pd
 script_dir = Path(__file__).parent
 df = pd.read_csv(script_dir / 'dirty_cafe_sales.csv')
 
-df.shape
-print("-" * 50) 
-for col in df.columns:
-    if df[col].nunique() < 20 :
-        print(f"\n--- {col} (Unique Count: {df[col].nunique()}) ---")
-        print(df[col].value_counts(dropna=False))
+# handel values
 
-print(col)
+cat_cols =['Item', 'Payment Method', 'Location']
+df[cat_cols] = df[cat_cols].fillna('Unknown')
+
+num_cols = ['Quantity', 'Price Per Unit', 'Total Spent']
+df[num_cols] = df[num_cols].apply(pd.to_numeric)
+
+# Fill missing values using math formula
+df['Quantity'] = df['Quantity'].fillna(df['Total Spent'] / df['Price Per Unit'])
+df['Price Per Unit'] = df['Price Per Unit'].fillna(df['Total Spent'] / df['Quantity'])
+df['Total Spent'] = df['Total Spent'].fillna(df['Quantity'] * df['Price Per Unit'])
+
+print(df.isna().sum())
+print("-" * 50) 
+print(df.shape)
+print("-" * 50) 
+
